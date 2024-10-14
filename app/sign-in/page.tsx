f44794e4c959a06f26e8768c10305d66e8885487
Shubhamss105 <shubhamsingh.ss.1407@gmail.com>
2024-10-14 22:44:03 +0530

    authentication bug fix

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,7 +1,9 @@
-// pages/sign-in.tsx
-
 import { SignIn } from "@clerk/nextjs";
 
-export default function SignInPage() {
-  return <SignIn />;
+export default function Page() {
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <SignIn />
+    </div>
+  );
 }
